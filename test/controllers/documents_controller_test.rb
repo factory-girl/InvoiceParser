9e64d57ce2594d0e9e0b8f13864127fa4d95189e
mckaylareilly <mckaylacreilly@gmail.com>
2021-02-22 07:30:24 -0330

    Removing incomplete spec

--- test/controllers/documents_controller_test.rb
+++ test/controllers/documents_controller_test.rb
@@ -4,15 +4,7 @@ class DocumentsControllerTest < ActionDispatch::IntegrationTest
   test "show" do
     test_pdf = Rails.root.join "test/fixtures/files/invoices_HubdocInvoice1.pdf"
     post uploads_path, params: { email: "mckaylacreilly@gmail.com", invoice: test_pdf }
-    get documents_path(Document.last.id)
-    assert_response :success
-  end
-
-  test "if the document has already been parsed, it does not parse the document again" do
-    test_pdf = Rails.root.join "test/fixtures/files/invoices_HubdocInvoice1.pdf"
-    post uploads_path, params: { email: "mckaylacreilly@gmail.com", invoice: test_pdf }
-    Document.last.update(vendor: "test")
-    get documents_path(Document.last.id)
+    get 'documents', params: { id: Document.last.id }
     assert_response :success
   end
 end
